663a6696c82201278d01766ded1b49b9cd123669
cqb13 <icaticati2.0@gmail.com>
2022-06-16 15:27:43 -0400

    getting rid of change sings

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let lives;
 let mode;
 let word;
 
+// TODO: depending on how much elements are updated, add a function that updates all elements, might make code easier to read.
+
 // utility controls
 helpButton.addEventListener("click", function(){
     document.getElementById("help").style.display = "block";
@@ -29,11 +31,17 @@ playButton.addEventListener("click", function(){
     start();
 });
 
+// TODO: make guess input clear when guess button is pressed
 guessButton.addEventListener("click", function(){
     game();
 });
 
 // TODO: something is very wrong with all this code, page reloads every time enter is pressed
+/**
+ * website is reloaded and all stats and words are cleared
+ * need to do some testing
+ * issue is only occurs when enter key is pressed, guess button works fine
+*/
 guess.addEventListener('keypress', (event) => {
     if(event.key == "Enter"){
         game();
@@ -42,6 +50,7 @@ guess.addEventListener('keypress', (event) => {
 
 // setup for the game
 function start(){
+    guesses = 0;
     if (document.getElementById("hard-mode").checked){
         lives = 6;
         mode = "hard";
@@ -54,6 +63,7 @@ function start(){
     }
     genWord(mode);
     wordReplace();
+    document.getElementById("stats-guesses").innerHTML = "- Guesses: " + guesses;
     document.getElementById("stats-word-length").innerHTML = "- Word length: " + word.length;
     document.getElementById("stats-lives").innerHTML = "- Lives: " + lives;
 }
@@ -74,6 +84,7 @@ function genWord(mode){
 }
 
 // replaces letters in the word with underscores. If your guess is in the word, it will replace the underscore coresponding with your guesses location in the word with your guess
+// TODO: seperate underscore by spaces to make it easier to see location of guess
 function wordReplace(){
     hiddenWord = word;
     for(var i = 0; i != hiddenWord.length; i++){
@@ -90,23 +101,24 @@ function wordReplace(){
 
 // controls the flow of the game
 function game(){
-    validGuess(valid)
+    valid = guessCheck();
     if (valid != false){
         guesses += 1;
         usedLetters.push(guess.value);
-        console.log(usedLetters);
     }
     document.getElementById("stats-guesses").innerHTML = "- Guesses: " + guesses;
     valid = true;
     wordReplace();
 }
 
-function validGuess(valid){
-    if ((guess.value).length == 1){
+// checks that your guess is 1 character long and is a letter and if your guess is in the word
+// if statement does not end in false return because full word check will be added after
+function guessCheck(){
+    if ((guess.value).length <= 1){
         for (var i = 0; i < usedLetters.length; i++){
-            if (guess == usedLetters[i]){
-              valid = false;
-              return valid;
+            if (guess.value == usedLetters[i]){
+                valid = false;
+                return valid;
             }
         }
     }
