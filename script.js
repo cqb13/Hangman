9b2fb155743f5101c08f2c0fe9670f30bb7bc509
cqb13 <icaticati2.0@gmail.com>
2022-06-16 09:06:48 -0400

    added the hidden word function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import {Words} from './words.js';
 
 const closeHelpBUtton = document.getElementById("close-button");
-const hiddenWordView = document.getElementById("word-view");
 const guessButton = document.getElementById("guess-button");
 const helpButton = document.getElementById("help-button");
 const playButton = document.getElementById("play-button");
@@ -29,16 +28,16 @@ playButton.addEventListener("click", function(){
 });
 
 guessButton.addEventListener("click", function(){
-    guess = guess.value;
-    game(guess);
+    game();
 });
 
 guess.addEventListener('keypress', (event) => {
     if(event.key == "Enter"){
-        game(guess);
+        game();
     }
 });
 
+// setup for the game
 function start(){
     if (document.getElementById("hard-mode").checked){
         lives = 6;
@@ -51,10 +50,12 @@ function start(){
         mode = "normal";
     }
     genWord(mode);
+    wordReplace();
     document.getElementById("stats-word-length").innerHTML = "- Word length: " + word.length;
     document.getElementById("stats-lives").innerHTML = "- Lives: " + lives;
 }
 
+// runs through words, until it finds a word that fits the requirments for the mode.
 function genWord(mode){
     word = Words[Math.round(Math.random() * Words.length)];
     if (mode == "hard") {
@@ -66,6 +67,24 @@ function genWord(mode){
             word = Words[Math.round(Math.random() * Words.length)];
         }
     }
-    console.log(word);
     return word;
+}
+
+// replaces letters in the word with underscores. If your guess is in the word, it will replace the underscore coresponding with your guesses location in the word with your guess
+function wordReplace(){
+    hiddenWord = word;
+    for(var i = 0; i != hiddenWord.length; i++){
+        hiddenWord = hiddenWord.split('');
+        if (guesses <= 0){
+            hiddenWord[i] = '_'
+        } else if (hiddenWord[i] == guess.value){
+            hiddenWord[i] = guess.value;
+        }
+        hiddenWord = hiddenWord.join('');
+    }
+    document.getElementById("word-view").innerHTML = hiddenWord;
+}
+
+function game() {
+    wordReplace();
 }
