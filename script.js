a560b10eb6a33e77b2b4a8f124450869584346dc
cqb13 <icaticati2.0@gmail.com>
2022-07-03 07:08:51 -0400

    delete radio btn theme

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ function reset(){
     usedLetters = [];
     valid = true;
     guesses = 0;
-    document.getElementById("stats-word-length").innerHTML = "- Word length: N/A";
+    document.getElementById("stats-word-length").innerHTML = "Word length: N/A";
     document.getElementById("stats-guesses").innerHTML = "Guesses: 0";
     document.getElementById("stats-lives").innerHTML = "Lives: N/A";
     document.getElementById("word-view").innerHTML = "Word";
